afb441cdf8463c4b93c08b312e350ed9844510d6
Aleksandra <naoki.kircia@gmail.com>
2020-09-06 22:31:02 +0200

    long time update

--- page_object_pattern/tests/test_hotel_search2.py
+++ page_object_pattern/tests/test_hotel_search2.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from page_object_pattern.pages.search_hotel import SearchHotelPage
 from page_object_pattern.pages.search_results import SearchResultsPage
+import allure
+# pytest --alluredir=E:\PythonProjects\Selenium_Apps\Result
 
 class TestHotelSearch:
 
@@ -14,6 +16,8 @@ class TestHotelSearch:
         yield
         self.driver.quit()
 
+    @allure.title('This is title')
+    @allure.description('Test description')
     def test_hotel_search(self, setup):
         self.driver.get("http://www.kurs-selenium.pl/demo/")
         search_hotel_page = SearchHotelPage(self.driver)
@@ -29,7 +33,11 @@ class TestHotelSearch:
         assert hotel_names[1] == 'Oasis Beach Tower'
         assert hotel_names[2] == 'Rose Rayhaan Rotana'
         assert hotel_names[3] == 'Hyatt Regency Perth'
-        assert hotel_prices[0] == '$22'
-        assert hotel_prices[1] == '$50'
-        assert hotel_prices[2] == '$80'
-        assert hotel_prices[3] == '$150'
+        # assert hotel_prices[0] == '$22'
+        # assert hotel_prices[1] == '$50'
+        # assert hotel_prices[2] == '$80'
+        # assert hotel_prices[3] == '$150'
+        assert hotel_prices[0] == '£14.30'
+        assert hotel_prices[1] == '£32.50'
+        assert hotel_prices[2] == '£52'
+        assert hotel_prices[3] == '£97.50'
